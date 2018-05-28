79bf171ce53042c0dfbf3362aa70a4d865900ebe
davidedc <davidedc@gmail.com>
2018-05-28 13:38:41 +0100

    refining the script so that all directories are removed before being copied over (otherwise cp -r just merges them)

--- copy-site-from-mmistakes-fork.sh
+++ copy-site-from-mmistakes-fork.sh
@@ -6,14 +6,32 @@ cp -r ../minimal-mistakes-fork-for-fizzygum-site/minimal-mistakes-jekyll.gemspec
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/Gemfile.lock .
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/Gemfile .
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/banner.js .
+
 rm -rf assets
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/assets .
+
+rm -rf _sass
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_sass .
+
+rm -rf _posts
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_posts .
+
+rm -rf _pages
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_pages .
+
+rm -rf _layouts
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_layouts .
+
+rm -rf _includes
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_includes .
+
+rm -rf _drafts
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_drafts .
+
+rm -rf _docs
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_docs .
+
+rm -rf _data
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_data .
+
 cp -r ../minimal-mistakes-fork-for-fizzygum-site/_config.yml .
